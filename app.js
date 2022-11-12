0d6eee2ec62d90d208e3a3cf95ba86c9e1172284
sleepypenguin763 <kenya_kimata+1300@brown.edu>
2022-11-11 22:29:10 -0500

    wireframes done

--- app.js
+++ app.js
@@ -38,6 +38,70 @@ const showSketches = (num) => {
   gallery.append(div);
 };
 
+const showWireframe = (num) => {
+  const gallery = document.getElementById("wireframe-selector");
+  if (document.body.contains(document.getElementById("wireframe-img"))) {
+    const child = document.getElementById("wireframe-img");
+    gallery.removeChild(child);
+  }
+
+  var br = document.createElement("br");
+
+  let div = document.createElement("div");
+  div.setAttribute("class", "text-center mx-auto");
+  div.setAttribute("style", "width:50%");
+  div.setAttribute("id", "wireframe-img");
+
+  let img = document.createElement("img");
+  switch(num){
+    case 1:
+      img.src = "img/lowfi_landing.jpg";
+      img.alt = "lowfi_landing.jpg";
+      break;
+    case 2:
+      img.src = "img/lowfi_messages.jpg";
+      img.alt = "lowfi_messages.jpg";
+      break;
+    case 3:
+      img.src = "img/lowfi_cc_home.png";
+      img.alt = "lowfi_cc_home.png";
+      break;
+    case 4:
+      img.src = "img/lowfi_cc_business.png";
+      img.alt = "lowfi_cc_business.png";
+      break;
+    case 5:
+      img.src = "img/lowfi_cc_profile.jpg";
+      img.alt = "lowfi_cc_profile.jpg";
+      break;
+    case 6:
+      img.src = "img/lowfi_cart.jpg";
+      img.alt = "lowfi_cart.jpg";
+      break;
+    case 7:
+      img.src = "img/lowfi_brand_home.jpg";
+      img.alt = "lowfi_brand_home.jpg";
+      break;
+    case 8:
+      img.src = "img/lowfi_brand_business.png";
+      img.alt = "lowfi_brand_business.png";
+      break;
+    case 9:
+      img.src = "img/lowfi_brand_profile.jpg";
+      img.alt = "lowfi_cc_profile.jpg";
+      break;
+    default:
+      img.src = "img/lowfi_team.jpg";
+      img.alt = "lowfi_team.jpg";
+      break;
+  }
+  img.setAttribute("class", "wireframe-img rounded mb-5 w-50");
+
+  div.append(br);
+  div.appendChild(img);
+  gallery.append(div);
+};
+
 const togglePhonePrototypeButton = () => {
   var button = document.getElementById("phone-prototype-button");
   button.textContent =
